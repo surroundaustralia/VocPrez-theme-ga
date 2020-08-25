4c0d164fb85936ffa4414e385578b023744e6e86
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-08-25 16:50:40 +1000

    abs path for all files

--- apply.sh
+++ apply.sh
@@ -6,9 +6,9 @@ cp $VP_THEME_HOME/templates/* $VP_HOME/vocprez/view/templates
 
 echo "Config: creating VocPrez config with $VP_THEME_HOME/config.py"
 echo "Alter config.py to include variables"
-sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' config.py > config_updated.py
-sed -i 's#$SPARQL_USERNAME#'"$SPARQL_USERNAME"'#' config_updated.py
-sed -i 's#$SPARQL_PASSWORD#'"$SPARQL_PASSWORD"'#' config_updated.py
+sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
+sed -i 's#$SPARQL_USERNAME#'"$SPARQL_USERNAME"'#' $VP_THEME_HOME/config_updated.py
+sed -i 's#$SPARQL_PASSWORD#'"$SPARQL_PASSWORD"'#' $VP_THEME_HOME/config_updated.py
 mv $VP_THEME_HOME/config_updated.py $VP_HOME/vocprez/_config/__init__.py
 
 echo "customisation done"
