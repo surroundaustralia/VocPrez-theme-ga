d69a1b88c14a3f7f611979fc1aa31b799b08223d
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-30 12:58:38 +1000

    VP 2.5

--- config.py
+++ config.py
@@ -1,22 +1,18 @@
 from os import path
-from os import environ
 
 APP_DIR = path.dirname(path.dirname(path.realpath(__file__)))
 SKIN_DIR = path.join(APP_DIR, "view")
 TEMPLATES_DIR = path.join(SKIN_DIR, "templates")
 STATIC_DIR = path.join(SKIN_DIR, "style")
-LOGFILE = APP_DIR + "/flask.log"
+LOGFILE = APP_DIR + "/vocprez.log"
 CACHE_FILE = path.join(APP_DIR, "cache", "DATA.p")
-VOCAB_CACHE_HOURS = (
-    1  # Number of hours before cache is replaced (set to zero to always replace)
-)
+CACHE_HOURS = 1
 DEFAULT_LANGUAGE = "en"
 SPARQL_QUERY_LIMIT = 2000  # Maximum number of results to return per SPARQL query
 MAX_RETRIES = 2
 RETRY_SLEEP_SECONDS = 10
 SPARQL_TIMEOUT = 60
-LOCAL_URLS = True  # Parameter governing whether URLs shown are local or external
-
+PORT = 5000
 
 #
 #   Vocabulary data sources
@@ -31,8 +27,10 @@ class VocabSource:
     VOCBENCH = "VOCBENCH"
     GITHUB = "GITHUB"
 
-# Main cache (SPARQL DB) variables
+
 # BEGIN Instance Vars
+SYSTEM_URI_BASE = "http://localhost:{}".format(PORT)
+USE_SYSTEM_URIS = True
 DEBUG = True
 SPARQL_ENDPOINT = "$SPARQL_ENDPOINT"
 SPARQL_USERNAME = "$SPARQL_USERNAME"
@@ -41,7 +39,7 @@ SOURCE_NAME = "ga"
 # END Instance Vars
 
 DATA_SOURCES = {
-    # example SPARQL source configured using varaibles in "Instance Vars" above
+    # example SPARQL source configured using variables in "Instance Vars" above
     SOURCE_NAME: {
         "source": VocabSource.SPARQL,
         "sparql_endpoint": SPARQL_ENDPOINT,
@@ -50,12 +48,10 @@ DATA_SOURCES = {
     },
 }
 
-# Details for the main vocabulary list. Can be overridden in the vocabularies.html template
 # BEGIN Vocabs list info
-VOCS_TITLE = "GGIC Vocabularies"
-VOCS_URI = "http://ggic.vocabs.ga.gov.au"
+VOCS_TITLE = "GA Vocabularies"
+VOCS_URI = "http://ga.vocabs.ga.gov.au"
 VOCS_DESC = "Vocabularies managed and published by Geoscience Australia on behalf of the Government Geoscience Information Committee (GGIC)"
-ADMIN_PWD = "vox"
 # END Vocabs list info
 
 # Admin functions
@@ -67,4 +63,5 @@ GITHUB_RAW_URI_BASE = "$GITHUB_RAW_URI_BASE"
 GITHUB_USR = None
 GITHUB_PWD = None
 GITHUB_TOKEN = "$GITHUB_TOKEN"
+ADMIN_PWD = "vox"
 # END Admin functions
