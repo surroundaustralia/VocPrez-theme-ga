7bb18eb2c8b66edae14a1d00cc55d0993f3af692
Nicholas Car <nicholas.car@surroundaustralia.com>
2020-11-30 13:40:37 +1000

    SPARQL U & P for VP 2.5

--- apply.sh
+++ apply.sh
@@ -10,8 +10,18 @@ echo "Config"
 echo "creating VocPrez config with $VP_THEME_HOME/config.py"
 echo "Alter config.py to include variables"
 sed 's#$SPARQL_ENDPOINT#'"$SPARQL_ENDPOINT"'#' $VP_THEME_HOME/config.py > $VP_THEME_HOME/config_updated.py
-sed -i 's#$SPARQL_USERNAME#'"$SPARQL_USERNAME"'#' $VP_THEME_HOME/config_updated.py
-sed -i 's#$SPARQL_PASSWORD#'"$SPARQL_PASSWORD"'#' $VP_THEME_HOME/config_updated.py
+if [ -z "$SPARQL_USERNAME" ]
+then
+      sed -i 's#$SPARQL_USERNAME#None#' $VP_THEME_HOME/config_updated.py
+else
+      sed -i 's#$SPARQL_USERNAME#'\"$SPARQL_USERNAME\"'#' $VP_THEME_HOME/config_updated.py
+fi
+if [ -z "$SPARQL_PASSWORD" ]
+then
+      sed -i 's#$SPARQL_PASSWORD#None#' $VP_THEME_HOME/config_updated.py
+else
+      sed -i 's#$SPARQL_PASSWORD#'\"$SPARQL_PASSWORD\"'#' $VP_THEME_HOME/config_updated.py
+fi
 sed -i 's#$GRAPHDB_REPO_ID#'"$GRAPHDB_REPO_ID"'#' $VP_THEME_HOME/config_updated.py
 sed -i 's#$GRAPH_DB_URI#'"$GRAPH_DB_URI"'#' $VP_THEME_HOME/config_updated.py
 sed -i 's#$GITHUB_RAW_URI_BASE#'"$GITHUB_RAW_URI_BASE"'#' $VP_THEME_HOME/config_updated.py
